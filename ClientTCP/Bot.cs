37b4e2372fe8fa57c05ec78bf3056f49acd127b5
kkawi <64330896+kkawi@users.noreply.github.com>
2020-04-30 13:55:09 +0300

    Add files via upload

--- ClientTCP/Bot.cs
+++ ClientTCP/Bot.cs
@@ -28,5 +28,244 @@ namespace ClientTCP
         }
 
         #endregion
+
+        public void BotMath()
+        {
+            #region math
+            Console.WriteLine("select math function to be calculated:");
+
+            string MathOperator = Console.ReadLine();
+
+            Console.WriteLine("Enter the two numbers you want to calculate:");
+
+            try
+            {
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+
+                int num1 = Convert.ToInt32(a);
+                int num2 = Convert.ToInt32(b);
+
+                switch (MathOperator)
+                {
+                    case "+":
+                        {
+                            Console.WriteLine($"[{MathOperator}] Calculation result: {num1 + num2}");
+                        }
+                        break;
+                    case "-":
+                        {
+                            Console.WriteLine($"[{MathOperator}] Calculation result: {num1 - num2}");
+                        }
+                        break;
+                    case "*":
+                        {
+                            Console.WriteLine($"[{MathOperator}] Calculation result: {num1 * num2}");
+                        }
+                        break;
+                    case "/":
+                        {
+                            Console.WriteLine($"[{MathOperator}] Calculation result: {num1 / num2}");
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("I don’t know such a mathematical operator (");
+                        }
+                        break;
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            #endregion
+        }
+        public void BotRandom()
+        {
+            #region random
+            string error = "[Error] ";
+
+            Console.WriteLine("Enter a number from:");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Enter number to:");
+            string b = Console.ReadLine();
+
+            try
+            {
+                int x = Convert.ToInt32(a);
+                int y = Convert.ToInt32(b);
+
+                var rnd = new Random();
+
+                if (x > y)
+                {
+                    Console.WriteLine(error + "Number from, greater than number to.");
+                }
+                if (x == y)
+                {
+                    Console.WriteLine(error + "You entered two identical numbers to get a random number... trolling???");
+                }
+                if (x < y)
+                {
+                    var result = rnd.Next(x, y);
+                    Console.WriteLine($"Random number ranging from {x} and to {y}: {result}.");
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            #endregion
+        }
+        public void BotTextColor()
+        {
+            #region textcolor
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("         Available colors for changing chat text color");
+            Console.WriteLine("----------------------------------------------------------------");
+
+            if (Console.ForegroundColor != ConsoleColor.White)
+            {
+                Console.WriteLine("The background / chat color will turn white at the time of selection");
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Red");
+            Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine("Cyan");
+            Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine("Yellow");
+            Console.ForegroundColor = ConsoleColor.Magenta; Console.WriteLine("Magenta");
+            Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Green");
+            Console.ForegroundColor = ConsoleColor.Blue; Console.WriteLine("Blue");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Enter the names of one of the colors you want to choose");
+            string Select = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(Select))
+            {
+                for (var i = 0; i == 0;)
+                {
+                    Console.WriteLine("Color cannot be empty, try entering again.");
+
+                    string ColorRepeat = Console.ReadLine();
+                    Select = ColorRepeat;
+
+                    if (ColorRepeat != "")
+                    {
+                        Select = ColorRepeat;
+                        break;
+                    }
+                }
+            }
+
+            if (Select.Equals("Red", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            if (Select.Equals("Cyan", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+            if (Select.Equals("Yellow", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            if (Select.Equals("Magenta", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+            }
+            if (Select.Equals("Green", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            if (Select.Equals("Blue", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            #endregion
+        }
+        public void BotBackgroundColor()
+        {
+            #region backgroundcolor
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("         Available colors for changing chat background:");
+            Console.WriteLine("----------------------------------------------------------------");
+
+            if (Console.BackgroundColor != ConsoleColor.White)
+            {
+                Console.WriteLine("The background / chat color will turn white at the time of selection");
+            }
+
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.BackgroundColor = ConsoleColor.Red; Console.WriteLine("Red");
+            Console.BackgroundColor = ConsoleColor.Cyan; Console.WriteLine("Cyan");
+            Console.BackgroundColor = ConsoleColor.Yellow; Console.WriteLine("Yellow");
+            Console.BackgroundColor = ConsoleColor.Magenta; Console.WriteLine("Magenta");
+            Console.BackgroundColor = ConsoleColor.Green; Console.WriteLine("Green");
+            Console.BackgroundColor = ConsoleColor.Blue; Console.WriteLine("Blue");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Enter the names of one of the colors you want to choose");
+            string Select = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(Select))
+            {
+                for (var i = 0; i == 0;)
+                {
+                    Console.WriteLine("Color cannot be empty, try entering again.");
+
+                    string ColorRepeat = Console.ReadLine();
+                    Select = ColorRepeat;
+
+                    if (ColorRepeat != "")
+                    {
+                        Select = ColorRepeat;
+                        break;
+                    }
+                }
+            }
+
+            if (Select.Equals("Red", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+            if (Select.Equals("Cyan", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+            }
+            if (Select.Equals("Yellow", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+            }
+            if (Select.Equals("Magenta", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.BackgroundColor = ConsoleColor.Magenta;
+            }
+            if (Select.Equals("Green", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+            }
+            if (Select.Equals("Blue", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+            }
+
+            #endregion
+        }
     }
 }
--- ClientTCP/Program.cs
+++ ClientTCP/Program.cs
@@ -69,8 +69,8 @@ namespace ClientTCP
 
                 if (Message.Equals("@bot /cmd", StringComparison.CurrentCultureIgnoreCase))
                 {
-                    Console.WriteLine($"[Bot: {bot.nameBot}] - my command: {Environment.NewLine}@bot /name - show your chat nickname.{Environment.NewLine}@bot /loglist - show error log.{Environment.NewLine}@bot /math (example) - math calculation.");
-                    Console.WriteLine($"@bot /clear - clear chat.{Environment.NewLine}@bot /tcolor - change chat text color.{Environment.NewLine}@bot /bcolor - change chat background color.{Environment.NewLine}@bot /rnd - random number in the specified range.{Environment.NewLine}@bot /reset - reset color text/background");
+                    Console.WriteLine($"[Bot: {bot.nameBot}] - my command: {Environment.NewLine}@bot /name - show your chat nickname.{Environment.NewLine}@bot /loglist - show error log.{Environment.NewLine}@bot /math (example) - math calculation.{Environment.NewLine}@bot /reset - reset color text/background.");
+                    Console.WriteLine($"@bot /clear - clear chat.{Environment.NewLine}@bot /tcolor - change chat text color.{Environment.NewLine}@bot /bcolor - change chat background color.{Environment.NewLine}@bot /rnd - random number in the specified range.");
                 }
 
                 if (Message.Equals("@bot /name", StringComparison.CurrentCultureIgnoreCase))
@@ -79,7 +79,7 @@ namespace ClientTCP
                 }
                 if (Message.Equals("@bot /rnd", StringComparison.CurrentCultureIgnoreCase))
                 {
-                    BotRandom();
+                    bot.BotRandom();
                     Console.WriteLine($"[Bot: {bot.nameBot}] - Random number regeneration completed");
                 }
                 if (Message.Equals("@bot /clear", StringComparison.CurrentCultureIgnoreCase))
@@ -89,12 +89,12 @@ namespace ClientTCP
                 }
                 if (Message.Equals($"@bot /math", StringComparison.CurrentCultureIgnoreCase))
                 {
-                    BotMath();
+                    bot.BotMath();
                     Console.WriteLine($"[Bot: {bot.nameBot}] - Bot completed the robot with computations");
                 }
                 if (Message.Equals($"@bot /tcolor", StringComparison.CurrentCultureIgnoreCase))
                 {
-                    BotTextColor();
+                    bot.BotTextColor();
                     Console.WriteLine($"[Bot: {bot.nameBot}] - Chat text color changed");
                 }
                 if (Message.Equals($"@bot /reset", StringComparison.CurrentCultureIgnoreCase))
@@ -104,7 +104,7 @@ namespace ClientTCP
                 }
                 if (Message.Equals($"@bot /bcolor", StringComparison.CurrentCultureIgnoreCase))
                 {
-                    BotBackgroundColor();
+                    bot.BotBackgroundColor();
                     Console.WriteLine($"[Bot: {bot.nameBot}] - Chat background color changed");
                 }
                 if (Message.Equals("@bot /loglist", StringComparison.CurrentCultureIgnoreCase))
@@ -149,245 +149,6 @@ namespace ClientTCP
 
                 tcpSocket.Shutdown(SocketShutdown.Both);
                 tcpSocket.Close();
-            }
-
-            #endregion
-        }
-
-        public static void BotMath()
-        {
-            #region math
-            Console.WriteLine("select math function to be calculated:");
-
-            string MathOperator = Console.ReadLine();
-
-            Console.WriteLine("Enter the two numbers you want to calculate:");
-
-            try
-            {
-                string a = Console.ReadLine();
-                string b = Console.ReadLine();
-
-                int num1 = Convert.ToInt32(a);
-                int num2 = Convert.ToInt32(b);
-
-                switch (MathOperator)
-                {
-                    case "+":
-                        {
-                            Console.WriteLine($"[{MathOperator}] Calculation result: {num1 + num2}");
-                        }
-                        break;
-                    case "-":
-                        {
-                            Console.WriteLine($"[{MathOperator}] Calculation result: {num1 - num2}");
-                        }
-                        break;
-                    case "*":
-                        {
-                            Console.WriteLine($"[{MathOperator}] Calculation result: {num1 * num2}");
-                        }
-                        break;
-                    case "/":
-                        {
-                            Console.WriteLine($"[{MathOperator}] Calculation result: {num1 / num2}");
-                        }
-                        break;
-                    default:
-                        {
-                            Console.WriteLine("I don’t know such a mathematical operator (");
-                        }
-                        break;
-                }
-            }
-            catch(FormatException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch(DivideByZeroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch(ArgumentNullException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            #endregion
-        }
-        public static void BotRandom()
-        {
-            #region random
-            string error = "[Error] ";
-
-            Console.WriteLine("Enter a number from:");
-            string a = Console.ReadLine();
-
-            Console.WriteLine("Enter number to:");
-            string b = Console.ReadLine();
-
-            try
-            {
-                int x = Convert.ToInt32(a);
-                int y = Convert.ToInt32(b);
-
-                var rnd = new Random();
-
-                if (x > y)
-                {
-                    Console.WriteLine(error + "Number from, greater than number to.");
-                }
-                if (x == y)
-                {
-                    Console.WriteLine(error + "You entered two identical numbers to get a random number... trolling???");
-                }
-                if (x < y)
-                {
-                    var result = rnd.Next(x, y);
-                    Console.WriteLine($"Random number ranging from {x} and to {y}: {result}.");
-                }
-            }
-            catch(FormatException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            #endregion
-        }
-        public static void BotTextColor()
-        {
-            #region textcolor
-            Console.WriteLine("----------------------------------------------------------------");
-            Console.WriteLine("         Available colors for changing chat text color");
-            Console.WriteLine("----------------------------------------------------------------");
-
-            if (Console.ForegroundColor != ConsoleColor.White)
-            {
-                Console.WriteLine("The background / chat color will turn white at the time of selection");
-            }
-
-            Console.ForegroundColor = ConsoleColor.White;
-
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Red");
-            Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine("Cyan");
-            Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine("Yellow");
-            Console.ForegroundColor = ConsoleColor.Magenta; Console.WriteLine("Magenta");
-            Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Green"); 
-            Console.ForegroundColor = ConsoleColor.Blue; Console.WriteLine("Blue");
-
-            Console.ForegroundColor = ConsoleColor.White;
-
-            Console.WriteLine("Enter the names of one of the colors you want to choose");
-            string Select = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(Select))
-            {
-                for (var i = 0; i == 0;)
-                {
-                    Console.WriteLine("Color cannot be empty, try entering again.");
-
-                    string ColorRepeat = Console.ReadLine();
-                    Select = ColorRepeat;
-
-                    if (ColorRepeat != "")
-                    {
-                        Select = ColorRepeat;
-                        break;
-                    }
-                }
-            }
-
-            if (Select.Equals("Red", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-            }
-            if (Select.Equals("Cyan", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-            }
-            if (Select.Equals("Yellow", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-            }
-            if (Select.Equals("Magenta", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.ForegroundColor = ConsoleColor.Magenta;
-            }
-            if (Select.Equals("Green", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-            }
-            if (Select.Equals("Blue", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.ForegroundColor = ConsoleColor.Blue;
-            }
-            #endregion
-        }
-        public static void BotBackgroundColor()
-        {
-            #region backgroundcolor
-
-            Console.WriteLine("----------------------------------------------------------------");
-            Console.WriteLine("         Available colors for changing chat background:");
-            Console.WriteLine("----------------------------------------------------------------");
-
-            if(Console.BackgroundColor != ConsoleColor.White)
-            {
-                Console.WriteLine("The background / chat color will turn white at the time of selection");
-            }
-
-            Console.BackgroundColor = ConsoleColor.Black;
-
-            Console.BackgroundColor = ConsoleColor.Red; Console.WriteLine("Red");
-            Console.BackgroundColor = ConsoleColor.Cyan; Console.WriteLine("Cyan");
-            Console.BackgroundColor = ConsoleColor.Yellow; Console.WriteLine("Yellow");
-            Console.BackgroundColor = ConsoleColor.Magenta; Console.WriteLine("Magenta");
-            Console.BackgroundColor = ConsoleColor.Green; Console.WriteLine("Green");
-            Console.BackgroundColor = ConsoleColor.Blue; Console.WriteLine("Blue");
-
-            Console.BackgroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Enter the names of one of the colors you want to choose");
-            string Select = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(Select))
-            {
-                for (var i = 0; i == 0;)
-                {
-                    Console.WriteLine("Color cannot be empty, try entering again.");
-
-                    string ColorRepeat = Console.ReadLine();
-                    Select = ColorRepeat;
-
-                    if (ColorRepeat != "")
-                    {
-                        Select = ColorRepeat;
-                        break;
-                    }
-                }
-            }
-
-            if (Select.Equals("Red", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-            }
-            if (Select.Equals("Cyan", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-            }
-            if (Select.Equals("Yellow", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.BackgroundColor = ConsoleColor.Yellow;
-            }
-            if (Select.Equals("Magenta", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.BackgroundColor = ConsoleColor.Magenta;
-            }
-            if (Select.Equals("Green", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-            }
-            if (Select.Equals("Blue", StringComparison.CurrentCultureIgnoreCase))
-            {
-                Console.BackgroundColor = ConsoleColor.Blue;
             }
 
             #endregion
